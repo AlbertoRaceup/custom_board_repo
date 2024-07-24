de39106fcaf708f0be6d8328f54ef89f2836f8c1
Alberto Damo <alberto.damo@gmail.com>
2024-07-24 08:26:12 +0200

    fixed package.sh script, now create the json and the bz2 tar file

--- ATC/extras/package.sh
+++ ATC/extras/package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=$(cat platform.txt | grep "version=" | cut -f2 -d"=")
+
 
 package_board(){
     git checkout boards.txt
@@ -10,7 +12,7 @@ package_board(){
     VARIANT=raceup
     EXCLUDE_TAGS=--exclude-tag-all=.portenta_only
 
-    FILENAME=ArduinoCore-renesas_$VARIANT-$VERSION.tar.bz2
+    FILENAME=Raceup-renesas_${VARIANT}-${VERSION}.tar.bz2
 
     CORE_BASE=`basename $PWD`
     cd ..
@@ -22,13 +24,13 @@ package_board(){
     CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
     SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
 
-    cat package_renesas_${VERSION}_index.json.tmp |
+    cat ./extras/package_raceup_VERSION_index.json.tmp |
         # sed "s/%%BUILD_NUMBER%%/${BUILD_NUMBER}/" |
             # sed "s/%%CURR_TIME%%/${CURR_TIME_SED}/" |
             sed "s/%%VERSION%%/${VERSION}/" |
-                sed "s/%%FILENAME_UNO%%/${FILENAME}/" |
-                sed "s/%%CHECKSUM_UNO%%/${CHKSUM}/" |
-                sed "s/%%SIZE_UNO%%/${SIZE}/" 
+                sed "s/%%FILENAME_BOARD%%/${FILENAME}/" |
+                sed "s/%%CHECKSUM_BOARD%%/${CHKSUM}/" |
+                sed "s/%%SIZE_BOARD%%/${SIZE}/" > package_raceup_${VERSION}_index.json
         }
 
 if [ ! -f platform.txt ]; then
@@ -44,7 +46,6 @@ git checkout boards.txt
 git checkout platform.txt
 
 
-VERSION=`cat platform.txt | grep "version=" | cut -f2 -d"="`
 echo $VERSION
 
 
@@ -52,58 +53,3 @@ package_board "atc"
 
 git checkout boards.txt
 git checkout platform.txt
-
-#atc
-
-# VARIANT=atc
-# EXCLUDE_TAGS=--exclude-tag-all=.portenta_only
-#
-# FILENAME=ArduinoCore-renesas_$VARIANT-$VERSION.tar.bz2
-#
-# CORE_BASE=`basename $PWD`
-# cd ..
-# tar $EXCLUDE_TAGS --exclude='*.vscode*' --exclude='*.tar.*' --exclude='*.json*' --exclude='*.git*' --exclude='*e2studio*' --exclude='*extras*' -cjhvf $FILENAME $CORE_BASE
-# cd -
-#
-# mv ../$FILENAME .
-#
-# CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
-# SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
-#
-# cat package_renesas_${VERSION}_index.json.tmp |
-# # sed "s/%%BUILD_NUMBER%%/${BUILD_NUMBER}/" |
-# # sed "s/%%CURR_TIME%%/${CURR_TIME_SED}/" |
-# sed "s/%%VERSION%%/${VERSION}/" |
-# sed "s/%%FILENAME_UNO%%/${FILENAME}/" |
-# sed "s/%%CHECKSUM_UNO%%/${CHKSUM}/" |
-# sed "s/%%SIZE_UNO%%/${SIZE}/" > package_renesas_${VERSION}_index.json
-#
-# cat package_renesas_${VERSION}_index.json
-#
-# VARIANT=smu
-# EXCLUDE_TAGS=--exclude-tag-all=.portenta_only
-#
-# FILENAME=ArduinoCore-renesas_$VARIANT-$VERSION.tar.bz2
-#
-# CORE_BASE=`basename $PWD`
-# cd ..
-# tar $EXCLUDE_TAGS --exclude='*.vscode*' --exclude='*.tar.*' --exclude='*.json*' --exclude='*.git*' --exclude='*e2studio*' --exclude='*extras*' -cjhvf $FILENAME $CORE_BASE
-# cd -
-#
-# mv ../$FILENAME .
-#
-# CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
-# SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
-#
-# cat package_renesas_${VERSION}_index.json.tmp |
-# # sed "s/%%BUILD_NUMBER%%/${BUILD_NUMBER}/" |
-# # sed "s/%%CURR_TIME%%/${CURR_TIME_SED}/" |
-# sed "s/%%VERSION%%/${VERSION}/" |
-# sed "s/%%FILENAME_UNO%%/${FILENAME}/" |
-# sed "s/%%CHECKSUM_UNO%%/${CHKSUM}/" |
-# sed "s/%%SIZE_UNO%%/${SIZE}/" > package_renesas_${VERSION}_index.json
-#
-# cat package_renesas_${VERSION}_index.json
-
-# git checkout boards.txt
-# git checkout platform.txt
