44f05bc90c6beee338a97152eb6671a6a48cf4c5
Alberto Damo <alberto.damo@gmail.com>
2024-08-03 11:22:38 +0200

    added new board: steering wheel board, fixed package script, now it will not do checkout before compacting

--- ATC/extras/package.sh
+++ ATC/extras/package.sh
@@ -2,54 +2,34 @@
 
 VERSION=$(cat platform.txt | grep "version=" | cut -f2 -d"=")
 
-
-package_board(){
-    git checkout boards.txt
-    git checkout platform.txt
-
-    sed -i 's/#$1./$1./g' boards.txt
-
-    VARIANT=raceup
-    EXCLUDE_TAGS=--exclude-tag-all=.portenta_only
-
-    FILENAME=Raceup-renesas_${VARIANT}-${VERSION}.tar.bz2
-
-    CORE_BASE=`basename $PWD`
-    cd ..
-    tar $EXCLUDE_TAGS --exclude='*.vscode*' --exclude='*.tar.*' --exclude='*.json*' --exclude='*.git*' --exclude='*e2studio*' --exclude='*extras*' -cjhvf $FILENAME $CORE_BASE
-    cd -
-
-    mv ../$FILENAME .
-
-    CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
-    SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
-
-    cat ./extras/package_raceup_VERSION_index.json.tmp |
-        # sed "s/%%BUILD_NUMBER%%/${BUILD_NUMBER}/" |
-            # sed "s/%%CURR_TIME%%/${CURR_TIME_SED}/" |
-            sed "s/%%VERSION%%/${VERSION}/" |
-                sed "s/%%FILENAME_BOARD%%/${FILENAME}/" |
-                sed "s/%%CHECKSUM_BOARD%%/${CHKSUM}/" |
-                sed "s/%%SIZE_BOARD%%/${SIZE}/" > package_raceup_${VERSION}_index.json
-        }
-
 if [ ! -f platform.txt ]; then
-  echo Launch this script from the root core folder as ./extras/package.sh
-  exit 2
+    echo Launch this script from the root core folder as ./extras/package.sh
+    exit 2
 fi
 
 if [ ! -d ../ArduinoCore-API ]; then
-  git clone https://github.com/arduino/ArduinoCore-API.git ../ArduinoCore-API
+    git clone https://github.com/arduino/ArduinoCore-API.git ../ArduinoCore-API
 fi
-
-git checkout boards.txt
-git checkout platform.txt
-
 
 echo $VERSION
 
+VARIANT=raceup
+EXCLUDE_TAGS=--exclude-tag-all=.portenta_only
 
-package_board "atc"
+FILENAME=Raceup-renesas_${VARIANT}-${VERSION}.tar.bz2
 
-git checkout boards.txt
-git checkout platform.txt
+CORE_BASE=`basename $PWD`
+cd ..
+tar $EXCLUDE_TAGS --exclude='*.vscode*' --exclude='*.tar.*' --exclude='*.json*' --exclude='*.git*' --exclude='*e2studio*' --exclude='*extras*' -cjhvf $FILENAME $CORE_BASE
+cd -
+
+mv ../$FILENAME .
+
+CHKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
+SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
+
+cat ./extras/package_raceup_VERSION_index.json.tmp |
+    sed "s/%%VERSION%%/${VERSION}/" |
+        sed "s/%%FILENAME_BOARD%%/${FILENAME}/" |
+        sed "s/%%CHECKSUM_BOARD%%/${CHKSUM}/" |
+        sed "s/%%SIZE_BOARD%%/${SIZE}/" > package_raceup_${VERSION}_index.json
